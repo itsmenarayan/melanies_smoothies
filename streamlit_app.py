a1237df75fe5fb4f9b60d1fd4f45dc4fca001509
Narayan Prajapati <Narayan.Prajapati@powerschool.com>
2025-07-07 17:34:43 +0530

    Update streamlit_app.py
    ingredients_string = " ".join(ingredients_list)

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,7 @@ if ingredients_list:
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
+        ingredients_string = " ".join(ingredients_list)
         
         # st.subheader(fruit_chosen + ' Nutrition Information')
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
