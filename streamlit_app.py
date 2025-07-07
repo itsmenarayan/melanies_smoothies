bd4bd1d76928ab8ec2e09efdc39dffa9b056fbd6
Narayan Prajapati <Narayan.Prajapati@powerschool.com>
2025-07-07 13:10:21 +0530

    Update streamlit_app.py
    temp

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,4 @@ VALUES ('{name_on_order}', '{ingredients_string.strip()}')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+        
